0b1e6808adcc725b0e1537e06fccbe72e9479ecb
yasely <yaselyespino@gmail.com>
2017-11-11 22:58:10 -0500

    laboratorio2

---
+++ ProyectoLaboratorio2/src/proyectolaboratorio2/ProyectoLaboratorio2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectolaboratorio2;
+
+/**
+ *
+ * @author Equipo
+ */
+public class ProyectoLaboratorio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
